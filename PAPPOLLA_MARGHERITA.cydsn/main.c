0efd7720a127d55365cb6a42f364b8e03e44fa19
“Marghe <meggy1797@gmail.com>
2020-11-11 11:17:54 +0100

    Primo codice funzionante

--- PAPPOLLA_MARGHERITA.cydsn/main.c
+++ PAPPOLLA_MARGHERITA.cydsn/main.c
@@ -39,7 +39,13 @@
 *   \brief Address of the Control register 4
 */
 #define LIS3DH_CTRL_REG4 0x23
-#define LIS3DH_CTRL_REG4_BDU_ACTIVE 0x88
+#define LIS3DH_CTRL_REG4_BDU_HR_ACTIVE 0x88
+
+#define STATUS_REG_ZYDA_HIGH 0x08
+#define OUT_X_L 0x28
+
+void ConvertAcceleration(uint8_t* DataToSend, uint8_t* DataRead);
+   
 
 int main(void)
 {
@@ -131,36 +137,39 @@ int main(void)
         error = I2C_Peripheral_ReadRegister(LIS3DH_DEVICE_ADDRESS,
                                         LIS3DH_CTRL_REG4,
                                         &ctrl_reg4);
-        if (error == NO_ERROR)
-    {
-        sprintf(message, "CTRL REGISTER 4: 0x%02X\r\n", ctrl_reg4);
-        UART_PutString(message); 
-    }
-    else
-    {
-        UART_PutString("Error occurred during I2C comm to read CTRL REG 4\r\n");   
-    }
-        if(ctrl_reg4!=LIS3DH_CTRL_REG4_BDU_ACTIVE){
-            ctrl_reg4=LIS3DH_CTRL_REG4_BDU_ACTIVE;
-            error=I2C_Peripheral_WriteRegister(LIS3DH_DEVICE_ADDRESS,
+        if (error == NO_ERROR){
+            if(ctrl_reg4!=LIS3DH_CTRL_REG4_BDU_HR_ACTIVE){
+                ctrl_reg4=LIS3DH_CTRL_REG4_BDU_HR_ACTIVE;
+                error=I2C_Peripheral_WriteRegister(LIS3DH_DEVICE_ADDRESS,
                                                 LIS3DH_CTRL_REG4,
                                                 ctrl_reg4);
-            if (error == NO_ERROR)
-            {
-                sprintf(message, "CTRL REGISTER 4: 0x%02X\r\n", ctrl_reg4);
-                UART_PutString(message); 
-            }
-            else
-            {
-                UART_PutString("Error occurred during I2C comm to write CTRL REG 4\r\n");   
-            }
+                if (error == NO_ERROR){
+                    sprintf(message, "CTRL REGISTER 4: 0x%02X\r\n", ctrl_reg4);
+                    UART_PutString(message); 
+                }
+                else{
+        
+                    UART_PutString("Error occurred during I2C comm to write CTRL REG 4\r\n");   
+                }
         }
+            
+        }
+        else
+        {
+            UART_PutString("Error occurred during I2C comm to read CTRL REG 4\r\n");   
+        }
+        
     
     
     /* Place your initialization/startup code here (e.g. MyInst_Start()) */
     uint8_t bits_ODR=0;
     uint8_t new_bits_ODR=0;
-    uint8_t new_ctrl_reg1=0;    
+    uint8_t new_ctrl_reg1=0; 
+    uint8_t OutArray[8];
+    uint8_t DataAcceleration[6];
+    OutArray[0]=0xA0;
+    OutArray[7]=0xC0;
+        
     for(;;)
     {
         if(flag_button_pressed){
@@ -201,9 +210,50 @@ int main(void)
                 UART_PutString("Error occurred during I2C comm to read control register 1\r\n");   
             }    
          
-            }
-        /* Place your application code here. */
     }
+        CyDelay(10);
+        uint8_t status_register; 
+        error = I2C_Peripheral_ReadRegister(LIS3DH_DEVICE_ADDRESS,
+                                        LIS3DH_STATUS_REG,
+                                        &status_register);
+    
+        if (error == NO_ERROR){
+            if(status_register & STATUS_REG_ZYDA_HIGH ){
+                error= I2C_Peripheral_ReadRegisterMulti(LIS3DH_DEVICE_ADDRESS,
+                                                        OUT_X_L,
+                                                        6, DataAcceleration);
+                if(error==NO_ERROR){
+                    ConvertAcceleration(&OutArray[1],&DataAcceleration[0]);
+                    ConvertAcceleration(&OutArray[3],&DataAcceleration[2]);
+                    ConvertAcceleration(&OutArray[5],&DataAcceleration[4]);
+                    UART_PutArray(OutArray,8);
+                    
+                }
+            }
+        
+    }
+    else
+    {
+        UART_PutString("Error occurred during I2C comm to read status register\r\n");   
+    }
+    
 }
+    }
+   
+ void ConvertAcceleration(uint8_t* DataToSend, uint8_t* DataRead)
+{
+    int16 out_acceleration;
+    out_acceleration= (int16) (*DataRead |(*(DataRead+1)<<8))>>4 ;
+    float sensitivity= 9.8/1000;
+    int16 trick=1000;
+    float out_acceleration_m_s2= out_acceleration*sensitivity;
+    out_acceleration= out_acceleration_m_s2*trick;
+    *(DataToSend)=(uint8_t) (out_acceleration & 0xFF);
+    *(DataToSend+1)= (uint8_t) (out_acceleration >>8);
+}
+            
 
+ 
+        
+        
 /* [] END OF FILE */
